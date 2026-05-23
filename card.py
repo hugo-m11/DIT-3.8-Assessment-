bd64556e224a8cf860f81d28a97abb42b54497d2
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-05-24 11:44:40 +1200

    Add files via upload

--- card.py
+++ card.py
@@ -1,4 +1,5 @@
 from tkinter import*
+import random
 
 class Card:
     def __init__(self, name, value, suit):
@@ -6,21 +7,70 @@ class Card:
         self.value = value
         self.suit = suit
 
-    def __str__(self):
-        return self.name
-    
+        def __str__(self):
+           return self.name
+        
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw(self):
+        card = draw_card()
+        if card:
+            self.hand.append(card)
+
+    def show_hand(self):
+        for card in self.hand:
+            print(card)
 
 deck = []
+
 
 suits = ["Flask", "Saber", "Stave", "Coin"]
 
 for suit in suits:
     for num in range(1, 12):
+# normal cards
         deck.append(Card(f"{num} of {suit}", num, suit))
-
+# face cards 
     deck.append(Card(f"{suit} Commander", 12, suit))
     deck.append(Card(f"{suit} Mistress", 13, suit))
     deck.append(Card(f"{suit} Master", 14, suit))
     deck.append(Card(f"Ace of {suit}", 15, suit))
-    deck.append(Card("Balance", -11, "Special"))
-    deck.append(Card("Demise", -13, "Special"))
+
+
+special_cards = [
+    ("Balance", -11),
+    ("Idiot", 0),
+    ("Endurance", -8),
+    ("Moderation", -14),
+    ("Evil One", -15),
+    ("Queen of Air and Darkness", -2),
+    ("Demise", -13),
+    ("Star", -17)
+]
+
+# adds two copies of each special card
+for name, value in special_cards:
+    for i in range(2):
+        deck.append(Card(name, value, "Special"))
+
+    
+def shuffle_deck():
+    random.shuffle(deck)
+
+
+def draw_card():
+    if len(deck) > 0:
+        return deck.pop()
+    return None
+
+    
+
+
+
+
+    
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from tkinter import*
+import card
+
+class Mainloop:
+    def __init__ (self):
+
+
 
 
         
     
+if __name__ == "__main__":
+
+    root = Tk()
+    gui = Mainloop(root)
+    root.title("Sabbac")
+    root.mainloop()
